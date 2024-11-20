28214894508378e12fed7d0282e1b2cc6dd2cede
Erbit <erbitservices@gmail.com>
2024-11-20 11:44:27 +0530

    mihir

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json({ limit: "50mb" }));
 // app.use(bodyParser.json()); 
 
 const corsOptions = {
-    origin:"https://erb-book-portal.netlify.app",
+    origin: `${base_url}` || "https://book-portal-admin.netlify.app",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
   };
