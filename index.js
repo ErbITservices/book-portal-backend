fc6f7e7c0d9d36e3666238d86e92edadbbdeaaa2
ErbITservices <jayminprajapati5959@gmail.com>
2024-11-23 13:41:43 +0530

    subject  api  push

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ const base_url= process.env.BASE;
 
 const cpu = os.cpus().length;
 
-
+if (cluster.isPrimary) {
+  for (let index = 0; index < cpu; index++) {
+    cluster.fork();
+  }
+} else {
 
 
 //configuer  env 
@@ -31,7 +35,7 @@ app.use(express.json({ limit: "50mb" }));
 // app.use(bodyParser.json()); 
 
 const corsOptions = {
-  origin: ["http://gpmbooks.com","http://localhost:5173","https://erb-book-portal.netlify.app","https://book-portal-admin.netlify.app"],
+  origin: ["http://localhost:5173","https://erb-book-portal.netlify.app","https://book-portal-admin.netlify.app"],
     
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
@@ -60,3 +64,4 @@ app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
 
+}
